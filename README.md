5d16aca0e42c3663891e460b8ce8aca03857e42c
mm1549 <mariomaas@gmail.com>
2023-09-08 05:17:58 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Probability Calculator
 
-This is the boilerplate for the Probability Calculator project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/scientific-computing-with-python/scientific-computing-with-python-projects/probability-calculator
+This is a Probability Calculator using boilerplate code from freeCodeCamp. The related Repl can be found under https://replit.com/@mm1549/fcc-project-probability-calculator
--- prob_calculator.py
+++ prob_calculator.py
@@ -3,6 +3,39 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+  def __init__(self,**balls):
+    self.contents = []
+    for key,value in balls.items():
+      self.contents.extend([key for i in range(value)])
+
+  def draw(self,remove):
+    removed = []
+    for i in range(min(remove, len(self.contents))):
+      rand = random.randrange(len(self.contents))
+      removed.append(self.contents.pop(rand))
+    return removed
+      
+      
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+  M = 0
+  for i in range(num_experiments):
+    experiment = copy.deepcopy(hat)
+    removedList = experiment.draw(num_balls_drawn)
+    removedObj = {}
+    for ball in removedList:
+      if ball in removedObj:
+        removedObj[ball] += 1
+      else:
+        removedObj[ball] = 1
+    included = True
+    for ball, n in expected_balls.items():
+      if ball not in removedObj or removedObj[ball] < n:
+        included = False
+
+    if included:
+      M += 1    
+
+  return M/num_experiments
+  
